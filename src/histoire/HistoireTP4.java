d4d72e40d7421d2076a464977db7204d3bf5f9cf
Marco781227 <114508826+Marco781227@users.noreply.github.com>
2022-11-15 15:56:09 +0100

    TP4

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -1,7 +1,9 @@
 package histoire;
 
 import personnages.Humain;
-import personnages.Commerçant;;
+import personnages.Commerçant;
+import personnages.Yakuza;
+import personnages.Ronin;
 
 public class HistoireTP4 {
 	
@@ -9,18 +11,30 @@ public class HistoireTP4 {
 		
 		Humain prof = new Humain ("Prof","kombucha",54);
 		
-		prof.direBonjour();
+		/*prof.direBonjour();
 		prof.acheter("une boisson", 12);
 		prof.boire();
 		prof.acheter("un jeu", 2);
-		prof.acheter("un kimono", 50);
+		prof.acheter("un kimono", 50);*/
 		
 		Commerçant marco = new Commerçant ("Marco",20);
 		
-		marco.direBonjour();
+		/*marco.direBonjour();
 		marco.seFaireExtorquer();
 		marco.recevoir(15);
-		marco.boire();
+		marco.boire();*/
+		
+		Yakuza yaku = new Yakuza ("Yaku le noir","whisky",30,"Warsong");
+		
+		/*yaku.direBonjour();
+		yaku.extorquer(marco);*/
+		
+		Ronin roro = new Ronin ("Roro","sochu",60);
+		
+		//roro.donner(marco);
+		
+		roro.provoquer(yaku);
+		
 		
 	}
 }
--- src/personnages/Commerçant.java
+++ src/personnages/Commerçant.java
@@ -8,10 +8,11 @@ public class Commerçant extends Humain {
 	
 	public int seFaireExtorquer() {
 		
+		int preArgent=argent;
 		argent=0;
 		parler("J'ai tout perdu! Le monde est trop injuste...");
 		
-		return 0;
+		return preArgent;
 		
 	}
 	
